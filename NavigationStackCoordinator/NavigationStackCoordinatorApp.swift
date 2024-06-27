5c824e164eedd2081581c86dd7468f542488fe09
Ilya Blinov <ilyablinov@icloud.com>
2024-06-27 07:50:56 +0300

    Initial Commit

---
+++ NavigationStackCoordinator/NavigationStackCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackCoordinatorApp.swift
+//  NavigationStackCoordinator
+//
+//  Created by Илья Блинов on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
